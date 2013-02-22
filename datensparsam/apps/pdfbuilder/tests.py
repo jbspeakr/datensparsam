9cd7f90d8bcbdadcf901269d113f3e18475a3f2d
jbspeakr <jbspeakr@funkblocka.de>
2013-02-22 23:06:52 +0100

    added fixture test

--- datensparsam/apps/pdfbuilder/tests.py
+++ datensparsam/apps/pdfbuilder/tests.py
@@ -1,15 +1,45 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 from django.test import TestCase
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+from datensparsam.apps.pdfbuilder.models import Municipality
+from datensparsam.apps.pdfbuilder.models import Recordsection
+
+
+class FixtureTest(TestCase):
+    def test_query_recordsection(self):
+        create_recordsection()
+        queryset = Recordsection.objects.filter(municipality='6531013')
+
+        for recordsection in queryset:
+            self.assertEqual(recordsection.zipcode, '35457')
+
+
+def create_recordsection():
+    """
+    Creates a municipality and the corresponding recordsection with ...
+    """
+    municipality = create_municipality()
+    return Recordsection.objects.create(
+        municipality=municipality,
+        address=u'Magistrat der Stadt Lollar',
+        city=u'Lollar',
+        zipcode=u'35457',
+        street=u'Holzmühler Weg 76'
+        )
+
+
+def create_municipality():
+    """
+    Creates a municipality with ...
+    """
+    return Municipality.objects.create(
+        key=u'6531013',
+        kind=u'Stadt',
+        district=u'Reg.-Bez. Gießen',
+        zipcode=u'35457',
+        state=u'Hessen',
+        name=u'Lollar, Stadt',
+        county=u'Gießen'
+        )
--- datensparsam/urls.py
+++ datensparsam/urls.py
@@ -1,13 +1,16 @@
 from django.conf.urls import patterns, include, url
 
+from datensparsam.apps.pdfbuilder.views import Recordsection
+
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns('datensparsam.urls',
     # Examples:
     # url(r'^$', 'datensparsam.views.home', name='home'),
     # url(r'^datensparsam/', include('datensparsam.foo.urls')),
+    url(r'^pdfbuilder/', views.hack, {}, name='domainhack'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
