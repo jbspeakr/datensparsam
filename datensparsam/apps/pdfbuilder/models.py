5423ca3cac975b5361f9fc3425ea7889c7aa3d7f
jbspeakr <jbspeakr@funkblocka.de>
2013-02-22 22:03:22 +0100

    added model comments

--- datensparsam/apps/pdfbuilder/models.py
+++ datensparsam/apps/pdfbuilder/models.py
@@ -1,20 +1,15 @@
 from django.db import models
 
 
-# class MunicipalityManager(models.Manager):
-#     def get_by_natural_key(self, key):
-#         return self.get(key=key)
-
-
 class Municipality(models.Model):
     ''' Gemeinde '''
-    key = models.CharField(max_length=16, primary_key=True)
-    kind = models.CharField(max_length=48)
-    district = models.CharField(max_length=48)
-    zipcode = models.CharField(max_length=6)
-    state = models.CharField(max_length=48)
-    name = models.CharField(max_length=48)
-    county = models.CharField(max_length=48)
+    key = models.CharField(max_length=16, primary_key=True)  # Gemeindeschlüssel
+    kind = models.CharField(max_length=48)  # Gemeindetyp
+    district = models.CharField(max_length=48)  # Regierungsbezirk
+    zipcode = models.CharField(max_length=6)  # PLZ
+    state = models.CharField(max_length=48)  # Bundesland
+    name = models.CharField(max_length=48)  # Gemeindenamen
+    county = models.CharField(max_length=48)  # Kreisname
 
     def __unicode__(self):
         return self.name
@@ -23,10 +18,10 @@ class Municipality(models.Model):
 class Recordsection(models.Model):
     ''' Meldestelle '''
     municipality = models.ForeignKey(Municipality)
-    address = models.CharField(max_length=200)
-    city = models.CharField(max_length=48)
-    zipcode = models.CharField(max_length=6)
-    street = models.CharField(max_length=48)
+    address = models.CharField(max_length=200)  # Anschrift
+    city = models.CharField(max_length=48)  # Ort
+    zipcode = models.CharField(max_length=6)  # PLZ
+    street = models.CharField(max_length=48)  # Straße
 
     def __unicode__(self):
         return self.address
