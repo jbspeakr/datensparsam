7a49a63fd11fbaaffdb40ee0b17e5fdab7bf53c9
jbspeakr <jbspeakr@funkblocka.de>
2013-03-09 13:56:29 +0100

    fix zipcodes in fixtures. minify images.

--- datensparsam/apps/pdfbuilder/forms/requestform.py
+++ datensparsam/apps/pdfbuilder/forms/requestform.py
@@ -12,10 +12,13 @@ class RequestForm(forms.Form):
     name = forms.CharField(max_length=32, label='Dein Nachname', widget=forms.TextInput(attrs={'placeholder': 'Mustermann'}))
     firstname = forms.CharField(max_length=32, label='Dein Vorname', widget=forms.TextInput(attrs={'placeholder': 'Maxi'}))
     address = forms.CharField(max_length=200, label='Deine Anschrift', widget=forms.TextInput(attrs={'placeholder': 'Lindenstraße 6'}))
-    zipcode = forms.IntegerField(label='Deine Postleitzahl', widget=forms.TextInput(attrs={'placeholder': '12345'}))
+    zipcode = forms.CharField(max_length=5, min_length=5, label='Deine Postleitzahl', widget=forms.TextInput(attrs={'placeholder': '12345'}))
     city = forms.CharField(max_length=32, label='Deine Stadt', widget=forms.TextInput(attrs={'placeholder': 'Berlin'}))
 
     def clean(self):
+        if not self.cleaned_data.get("zipcode").__len__() == 5:
+            raise forms.ValidationError('Wrong zipcode length.')
+
         if not self.is_valid_zipcode(self.cleaned_data.get("zipcode"), self.cleaned_data.get("state")):
             raise forms.ValidationError(_('Zipcode not in state.'))
         return self.cleaned_data
