e78d362eb5b02ee07c8a28fbc05f6b0faba18619
jbspeakr <jbspeakr@funkblocka.de>
2013-03-25 15:55:33 +0100

    adds rudimentary tastypie functionality

---
+++ datensparsam/apps/api/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, include
+from tastypie.api import Api
+from datensparsam.apps.api import api
+
+v1_api = Api(api_name='v1')
+v1_api.register(api.RegistrationOfficeResource())
+v1_api.register(api.MunicipalityResource())
+
+urlpatterns = patterns(
+    '',
+    (r'^', include(v1_api.urls)),
+)
--- datensparsam/settings.py
+++ datensparsam/settings.py
@@ -126,6 +126,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'datensparsam.apps.pdfbuilder',
+    'datensparsam.apps.api',
     'datensparsam.libs.pdf',
     'tastypie'
 )
