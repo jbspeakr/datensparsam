6019058f752482b3c4048282db46d4119d2c3c3d
jbspeakr <jbspeakr@funkblocka.de>
2013-03-29 10:44:14 +0100

    adds new design parts

--- datensparsam/apps/pdfbuilder/urls.py
+++ datensparsam/apps/pdfbuilder/urls.py
@@ -3,8 +3,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns(
     'datensparsam.apps.pdfbuilder.views',
-    # Examples:
-    # url(r'^$', 'datensparsam.views.home', name='home'),
-    # url(r'^datensparsam/', include('datensparsam.foo.urls')),
     url(r'^$', 'index', {}, name='index'),
 )
---
+++ static/js/main.js
@@ -0,0 +1,12 @@
+var _paq = _paq || [];
+_paq.push(["setDoNotTrack", true]);
+_paq.push(["trackPageView"]);
+_paq.push(["enableLinkTracking"]);
+
+(function() {
+var u=(("https:" == document.location.protocol) ? "https" : "http") + "://jbr.norma.uberspace.de/piwik/";
+_paq.push(["setTrackerUrl", u+"piwik.php"]);
+_paq.push(["setSiteId", "2"]);
+var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
+g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
+})();
