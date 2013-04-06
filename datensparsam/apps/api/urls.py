54556f678206b42897f1d2a7bfc9186b84e432b9
jbspeakr <jbspeakr@funkblocka.de>
2013-04-06 18:31:04 +0200

    adds corrected fixtures and extends api ressources

--- datensparsam/apps/api/urls.py
+++ datensparsam/apps/api/urls.py
@@ -5,6 +5,7 @@ from datensparsam.apps.api import api
 v1_api = Api(api_name='v1')
 v1_api.register(api.RegistrationOfficeResource())
 v1_api.register(api.MunicipalityResource())
+v1_api.register(api.ZipcodeResource())
 
 urlpatterns = patterns(
     '',
