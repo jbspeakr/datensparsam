22703fe63550241cd63a7dbdf0d81a4e94d6403b
jbspeakr <jbspeakr@funkblocka.de>
2013-04-08 10:11:46 +0200

    adds test cases

--- datensparsam/apps/api/tests.py
+++ datensparsam/apps/api/tests.py
@@ -28,15 +28,15 @@ class ApiTest(TestCase):
         self.failUnlessEqual(response.status_code, status)
         return response
 
-    def test_plain_sql_access(self):
+    def test_m2m_sql_access(self):
         zipcode = models.Zipcode.objects.get(id=1)
         municipalities = zipcode.municipalities.all()
-        registration_offices = zipcode.registration_offices.all()
+        registration_offices = zipcode.registrationoffices.all()
 
         self.assertTrue(municipalities)
         self.assertTrue(registration_offices)
 
-    def test_json_api_access(self):
+    def test_m2m_api_access(self):
         url = '/api/v1/zipcode/?format=json'
         response = self.GET(url)
         newJson = json.loads(response.content, encoding='utf-8')
@@ -47,3 +47,38 @@ class ApiTest(TestCase):
 
         self.assertTrue('zipcode' in zipcode)
         self.assertTrue('municipalities' in zipcode)
+
+    def test_municipality_api_access(self):
+        url = '/api/v1/municipality/?format=json'
+        response = self.GET(url)
+        newJson = json.loads(response.content, encoding='utf-8')
+
+        self.assertTrue(newJson)
+
+        municipality = newJson['objects'][0]
+
+        self.assertTrue('city' in municipality)
+        self.assertTrue('id' in municipality)
+        self.assertTrue('key' in municipality)
+        self.assertTrue('lat' in municipality)
+        self.assertTrue('lng' in municipality)
+        self.assertTrue('name' in municipality)
+        self.assertTrue('street' in municipality)
+        self.assertTrue('zipcode' in municipality)
+
+    def test_registrationoffice_api_access(self):
+        url = '/api/v1/registration-office/?format=json'
+        response = self.GET(url)
+        newJson = json.loads(response.content, encoding='utf-8')
+
+        self.assertTrue(newJson)
+
+        municipality = newJson['objects'][0]
+
+        self.assertTrue('city' in municipality)
+        self.assertTrue('id' in municipality)
+        self.assertTrue('lat' in municipality)
+        self.assertTrue('lng' in municipality)
+        self.assertTrue('name' in municipality)
+        self.assertTrue('street' in municipality)
+        self.assertTrue('zipcode' in municipality)
