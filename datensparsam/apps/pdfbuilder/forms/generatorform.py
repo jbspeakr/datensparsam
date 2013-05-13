d7a5b5c70e1586a2d89fad96b843ca0514882906
jbspeakr <jbspeakr@funkblocka.de>
2013-05-13 09:31:41 +0200

    adds required attributes to form fields. fixes recipient check.

--- datensparsam/apps/pdfbuilder/forms/generatorform.py
+++ datensparsam/apps/pdfbuilder/forms/generatorform.py
@@ -1,37 +1,61 @@
 from django import forms
+from django.utils.translation import ugettext_lazy as _
+
 from datensparsam.apps.api import models as apimodels
 
 
 class GeneratorForm(forms.Form):
     zipcode = forms.CharField(
         max_length=5,
-        widget=forms.TextInput(attrs={'placeholder': '12051'}))
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': '12051',
+                'required': 'true'
+            }))
     registrationoffice = forms.CharField(
         required=False)
     municipality = forms.CharField(
         required=False)
     name = forms.CharField(
         max_length=32,
-        widget=forms.TextInput(attrs={'placeholder': 'Mustermann'}))
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'Mustermann',
+                'required': 'true'
+            }))
     firstname = forms.CharField(
         max_length=32,
-        widget=forms.TextInput(attrs={'placeholder': 'Maxi'}))
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'Maxi',
+                'required': 'true'
+            }))
     address = forms.CharField(
         max_length=64,
-        widget=forms.TextInput(attrs={'placeholder': 'Lindenstr. 6'}))
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'Lindenstr. 6',
+                'required': 'true'
+            }))
     city = forms.CharField(
         max_length=64,
-        widget=forms.TextInput(attrs={'placeholder': 'Berlin'}))
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'Berlin',
+                'required': 'true'
+            }))
 
     def clean(self):
         if self.has_registrationoffice() or self.has_municipality():
             return self.cleaned_data
         else:
-            raise forms.ValidationError("No recipient found.")
+            raise forms.ValidationError(_("No recipient found."))
 
     def has_registrationoffice(self):
         try:
             data = self.cleaned_data['registrationoffice']
+            if data == '':
+                return False
             apimodels.RegistrationOffice.objects.get(id=data)
             return True
         except:
@@ -40,6 +64,8 @@ class GeneratorForm(forms.Form):
     def has_municipality(self):
         try:
             data = self.cleaned_data['municipality']
+            if data == '':
+                return False
             apimodels.Municipality.objects.get(id=data)
             return True
         except apimodels.Municipality.DoesNotExist:
