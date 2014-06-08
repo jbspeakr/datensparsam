b4b86ee7ea8e87cd59728271b9df487cb86f0efd
jbspeakr <jan@brennenstuhl.me>
2014-06-08 16:15:33 +0200

    remove deprecated pip parameter and simplify setup.py

--- setup.py
+++ setup.py
@@ -1,37 +1,13 @@
-import os
-from setuptools import setup
-import codecs
-import re
-from os import path
-
-
-README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
-
-# allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
-
-
-def read(*parts):
-    file_path = path.join(path.dirname(__file__), *parts)
-    return codecs.open(file_path).read()
+from distutils.core import setup
 
 setup(
     name='datensparsam',
     version='0.3',
-    packages=['datensparsam'],
-    license='MIT License',
-    description='Django-based civic app to opt-out at governmental record sections',
-    long_description=read('README.md'),
+    packages=['datensparsam', 'datensparsam.apps', 'datensparsam.apps.api', 'datensparsam.apps.pdfbuilder',
+              'datensparsam.apps.pdfbuilder.forms'],
     url='https://www.datensparsam.de',
+    license='MIT License',
     author='Jan Brennenstuhl',
     author_email='jan@brennenstuhl.me',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP',
-    ],
+    description='Django-based civic app to opt-out at governmental record sections.'
 )
