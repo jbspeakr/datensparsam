a15daa55cd9d928dc3f4657ccb01d9d83b9e2dad
jbspeakr <jan@brennenstuhl.me>
2015-01-22 20:25:29 +0100

    adapt setup file for new app.

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from distutils.core import setup
+from pip.req import parse_requirements
+
+requirements = parse_requirements('requirements.txt')
+requirements_list = [str(ir.req) for ir in requirements]
 
 setup(
     name='datensparsam',
-    version='0.4',
-    packages=['datensparsam', 'datensparsam.apps', 'datensparsam.apps.api', 'datensparsam.apps.pdfbuilder',
-              'datensparsam.apps.pdfbuilder.forms'],
+    version='1.0',
+    packages=['datensparsam'],
     url='https://www.datensparsam.de',
     license='MIT License',
     author='Jan Brennenstuhl',
     author_email='jan@brennenstuhl.me',
-    description='Django-based civic app to opt-out at governmental record sections.'
+    description='Python-based civic app to opt-out at governmental record sections.',
+    long_description=open('README.md').read() + '\n\n' + open('CHANGES.md').read(),
+    install_requires=requirements_list
 )
