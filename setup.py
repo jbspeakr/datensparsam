005650130634e91c3c6ee02dc415297724544147
jbspeakr <jan@brennenstuhl.me>
2015-01-22 20:50:44 +0100

    remove unknown property.

--- setup.py
+++ setup.py
@@ -1,8 +1,4 @@
 from distutils.core import setup
-from pip.req import parse_requirements
-
-requirements = parse_requirements('requirements.txt')
-requirements_list = [str(ir.req) for ir in requirements]
 
 setup(
     name='datensparsam',
@@ -13,6 +9,5 @@ setup(
     author='Jan Brennenstuhl',
     author_email='jan@brennenstuhl.me',
     description='Python-based civic app to opt-out at governmental record sections.',
-    long_description=open('README.md').read() + '\n\n' + open('CHANGES.md').read(),
-    install_requires=requirements_list
+    long_description=open('README.md').read() + '\n\n' + open('CHANGES.md').read()
 )
