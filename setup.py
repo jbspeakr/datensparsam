91abf39e5ab1a5f035a84e854364ccb8ffd0b9dc
jbspeakr <jan@brennenstuhl.me>
2015-01-26 22:24:36 +0100

    now also serving long_description.

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
 from distutils.core import setup
+import codecs
+import os
+
+
+def read(*parts):
+    filename = os.path.join(os.path.dirname(__file__), *parts)
+    with codecs.open(filename, encoding='utf-8') as fp:
+        return fp.read()
 
 setup(
     name='datensparsam',
@@ -9,5 +17,6 @@ setup(
     license='MIT License',
     author='Jan Brennenstuhl',
     author_email='jan@brennenstuhl.me',
-    description='Django-based civic app to opt-out at governmental record sections.'
+    description='A Privacy Empowerment App for German citizen written in Python using Django.',
+    long_description=read('README.md')
 )
